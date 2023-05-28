24d699275676cf9748fe8bbe4741981d6cdf75fd
Adelio <alfonzoadelio@gmail.com>
2023-05-28 01:13:06 -0300

    firtsCommit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const http=require("http");
+const exportsFromAnother=require("./another")
+console.log({exportsFromAnother})
+
+function resquestController(){
+    console.log("Hola Mudo!!");
+}
+
+
+//configuracion del servidor
+const server=http.createServer(resquestController)
+server.listen(4000)
